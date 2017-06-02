821bcb47b7d3c39474e4f0422ea3b16d431681f2
houfutian@fotoable.com <houfutian@fotoable.com>
2017-06-02 12:04:47 +0800

    Initial commit

---
+++ app/src/main/java/com/opengl/myopenglapplication/OpenGLRenderer.java
@@ -0,0 +1,72 @@
+package com.opengl.myopenglapplication;
+
+import android.opengl.GLSurfaceView;
+import android.opengl.GLU;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by fotoable on 2017/6/2.
+ */
+
+class OpenGLRenderer implements GLSurfaceView.Renderer {
+
+    /**
+     * 这个方法中主要用来设置一些绘制时不常变化的参数，比如：背景色，是否打开 z-buffer等。
+     * @param gl
+     * @param config
+     */
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        // Set the background color to black ( rgba ).
+        gl.glClearColor(0.0f, 0.0f, 0.0f, 0.5f);
+        // Enable Smooth Shading, default not really needed.
+        gl.glShadeModel(GL10.GL_SMOOTH);
+        // Depth buffer setup.
+        gl.glClearDepthf(1.0f);
+        // Enables depth testing.
+        gl.glEnable(GL10.GL_DEPTH_TEST);
+        // The type of depth testing to do.
+        gl.glDepthFunc(GL10.GL_LEQUAL);
+        // Really nice perspective calculations.
+        gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT,
+                GL10.GL_NICEST);
+    }
+
+    /**
+     * 如果设备支持屏幕横向和纵向切换，这个方法将发生在横向<->纵向互换时。此时可以重新设置绘制的纵横比率。
+     * @param gl
+     * @param width
+     * @param height
+     */
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        // Sets the current view port to the new size.
+        gl.glViewport(0, 0, width, height);
+        // Select the projection matrix
+        gl.glMatrixMode(GL10.GL_PROJECTION);
+        // Reset the projection matrix
+        gl.glLoadIdentity();
+        // Calculate the aspect ratio of the window
+        GLU.gluPerspective(gl, 45.0f,
+                (float) width / (float) height,
+                0.1f, 100.0f);
+        // Select the modelview matrix
+        gl.glMatrixMode(GL10.GL_MODELVIEW);
+        // Reset the modelview matrix
+        gl.glLoadIdentity();
+    }
+
+    /**
+     * 定义实际的绘图操作。
+     * @param gl
+     */
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        // Clears the screen and depth buffer.
+        gl.glClear(GL10.GL_COLOR_BUFFER_BIT |
+                GL10.GL_DEPTH_BUFFER_BIT);
+    }
+}
---
+++ app/src/main/java/com/opengl/myopenglapplication/TestActivity.java
@@ -0,0 +1,35 @@
+package com.opengl.myopenglapplication;
+
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by fotoable on 2017/6/2.
+ */
+
+public class TestActivity extends Activity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.test_act);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE); // (NEW)
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); // (NEW)
+
+
+        GLSurfaceView view = new GLSurfaceView(this);
+        view.setRenderer(new OpenGLRenderer());
+        setContentView(view);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
